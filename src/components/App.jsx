00ba651ff74a823b7315c8e8770a30e658db7712
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-07-23 21:30:34 +0300

    add input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Input from './Input/Input';
+import Container from './Input/Container';
+export default class App extends Component {
+  state = {
+    // contacts: [],
+    name: '',
+  };
+  render() {
+    return (
+      <Container>
+        <Input />
+      </Container>
+    );
+  }
+}
---
+++ src/components/Input/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Input = () => {
+  return (
+    <>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    </>
+  );
+};
+
+export default Input;
